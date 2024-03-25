93b7a6a17253ea743a7e32f74788a7c24791f49a
CarlosManuel2505 <163787386+CarlosManuel2505@users.noreply.github.com>
2024-03-24 18:17:17 -0600

    Add files via upload

---
+++ Nombre.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char nombre[20];
+	printf("Ingresa tu nombre\n");
+	fgets(nombre, sizeof(nombre), stdin);
+	printf("\n%s", nombre);
+	return 0;
+}
+
---
+++ if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int n1,n2;
+	n1=10;
+	n2=20;
+	if (n1>n2) {
+		printf("El número 1 es mayor que el número 2.");
+	}
+	else (n1<n2);
+	{
+		printf("El número 2 es mayor que el número 1.");
+	}
+	return 0;
+}
+
